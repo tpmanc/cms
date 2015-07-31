c9ddd28f09b851f9f33311c10706cd875255bffd
tpmanc <tpxtrime@mail.ru>
2015-07-31 17:26:05 +0300

    fix left menu

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -24,25 +24,27 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<nav>
-    <div class="user">
-        <i class="material-icons">account_circle</i> <?= Yii::$app->user->identity->username?> (выйти)
-    </div>
-    <ul>
-        <li><a href="<?= Url::to(['/core/static-page/index'])?>"><i class="material-icons">insert_drive_file</i> Static Pages</a></li>
-        <!-- <li><a href="#"><i class="material-icons">format_align_center</i> Статьи</a></li> -->
-        <li><a href="<?= Url::to(['/core/category/index'])?>"><i class="material-icons">list</i> Categories</a></li>
-        <li><a href="<?= Url::to(['/core/product/index'])?>"><i class="material-icons">local_grocery_store</i> Products</a></li>
-        <li><a href="<?= Url::to(['/core/product-rests/index'])?>"><i class="material-icons"></i> Products Rests</a></li>
-        <!-- <li><a href="#"><i class="material-icons">settings_ethernet</i> UML / XML</a></li> -->
-        <!-- <li><a href="#"><i class="material-icons">textsms</i> Отзывы</a></li> -->
-        <li><a href="<?= Url::to(['/core/order/index'])?>"><i class="material-icons"></i> Orders</a></li>
-        <li><a href="<?= Url::to(['/core/menu/index'])?>"><i class="material-icons"></i> Menu Builder</a></li>
-        <li><a href="<?= Url::to(['/core/delivery-type/index'])?>"><i class="material-icons"></i> Delivery Types</a></li>
-        <li><a href="<?= Url::to(['/core/payment-type/index'])?>"><i class="material-icons"></i> Payment Types</a></li>
-        <li><a href="<?= Url::to(['/core/user/index'])?>"><i class="material-icons">group</i> Users</a></li>
-    </ul>
-</nav>
+<?php if (!Yii::$app->user->isGuest) { ?>
+    <nav>
+        <div class="user">
+            <i class="material-icons">account_circle</i> <?= Yii::$app->user->identity->username?> (logout)
+        </div>
+        <ul>
+            <li><a href="<?= Url::to(['/core/static-page/index'])?>"><i class="material-icons">insert_drive_file</i> Static Pages</a></li>
+            <!-- <li><a href="#"><i class="material-icons">format_align_center</i> Статьи</a></li> -->
+            <li><a href="<?= Url::to(['/core/category/index'])?>"><i class="material-icons">list</i> Categories</a></li>
+            <li><a href="<?= Url::to(['/core/product/index'])?>"><i class="material-icons">local_grocery_store</i> Products</a></li>
+            <li><a href="<?= Url::to(['/core/product-rests/index'])?>"><i class="material-icons"></i> Products Rests</a></li>
+            <!-- <li><a href="#"><i class="material-icons">settings_ethernet</i> UML / XML</a></li> -->
+            <!-- <li><a href="#"><i class="material-icons">textsms</i> Отзывы</a></li> -->
+            <li><a href="<?= Url::to(['/core/order/index'])?>"><i class="material-icons"></i> Orders</a></li>
+            <li><a href="<?= Url::to(['/core/menu/index'])?>"><i class="material-icons"></i> Menu Builder</a></li>
+            <li><a href="<?= Url::to(['/core/delivery-type/index'])?>"><i class="material-icons"></i> Delivery Types</a></li>
+            <li><a href="<?= Url::to(['/core/payment-type/index'])?>"><i class="material-icons"></i> Payment Types</a></li>
+            <li><a href="<?= Url::to(['/core/user/index'])?>"><i class="material-icons">group</i> Users</a></li>
+        </ul>
+    </nav>
+<?php } ?>
 
 <div class="wrapper">
 
