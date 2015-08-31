4d59d56309eb1110761b1e09cb73c36b27404b46
Chukancev Nikita <tpxtrime@mail.ru>
2015-08-31 14:34:22 +0300

    Update params.php

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,9 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    
+    // product image sizes
+    'productBig' => ['width' => 428, 'height' => 380],
+    'productMedium' => ['width' => 218, 'height' => 200],
+    'productSmall' => ['width' => 90, 'height' => 78],
 ];
